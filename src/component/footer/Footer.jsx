e36ee3f41e96557bcdbadf312363fe705b30bcaf
kanchal vinod <68420824+kanchal-vinod@users.noreply.github.com>
2022-08-19 22:56:34 +0530

    newscard loading options added

---
+++ src/component/footer/Footer.jsx
@@ -0,0 +1,20 @@
+import "./footer.scss"
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <span className="name"><i>Developed by -</i>  {""}
+            <a className="namea" href="https://www.linkedin.com/in/kanchal-vinod-wijesooriya-46a8031ba/">
+               <b> Kanchal Vinod</b> 
+            </a>
+            </span>
+    
+            <hr />
+         
+            
+        </div>
+    );
+}
+
+export default Footer;
---
+++ src/component/navbar/Nav.jsx
@@ -0,0 +1,17 @@
+import './navbar.scss'
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <div className="nav">
+            <div className="icon">
+                
+            </div>
+            <div className="img"> <a href="https://www.serendib.io/" target="_blank"><img className="img" src="https://www.serendib.io/assets/images/logo/logo-main.png" alt="" /></a></div>
+            
+           
+        </div>
+    );
+}
+
+export default Nav;
